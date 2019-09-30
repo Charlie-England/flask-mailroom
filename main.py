f4d7bece18cb35130cf6826f8d03e09571325e75
Charlie England <charlie265@live.com>
2019-09-30 13:50:03 -0700

    Initial commit and changes updated base by adding navigation created create.jinja2 template updated main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import base64
 
 from flask import Flask, render_template, request, redirect, url_for, session
 
-from model import Donation 
+from model import Donation, Donor, db
 
 app = Flask(__name__)
 
@@ -15,6 +15,17 @@ def home():
 def all():
     donations = Donation.select()
     return render_template('donations.jinja2', donations=donations)
+
+@app.route('/create', methods = ['GET','POST'])
+def create():
+    if request.method == "POST":
+        db.connect()
+        name = request.form['name-input']
+        donation_amt = request.form['donation-input']
+        Donation.insert(value = donation_amt, donor = Donor.select().where(Donor.name == name)).execute()
+        return redirect(url_for('home'))
+    else:
+        return render_template('create.jinja2')
     
 
 if __name__ == "__main__":
